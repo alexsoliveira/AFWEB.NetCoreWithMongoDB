57247e3b5e9b91a076cc63705d395e73d2729e8e
alexsoliveira <alexsoliveira@gmail.com>
2024-06-22 17:01:52 -0300

    first commit

---
+++ src/EFCoreMongoConnect/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace EFCoreMongoConnet;
+
+public class Product
+{
+    public string Id { get; set;}
+    public required string Name { get; set;}
+    public required string Description { get; set;}
+    public required double Price { get; set;}
+}
